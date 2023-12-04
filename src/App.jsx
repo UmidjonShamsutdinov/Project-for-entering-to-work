aeda108145a9d10cd12058acf009ac84affffe58
UmidjonShamsutdinov <umid.2825@gmail.com>
2023-12-04 16:52:30 +0500

    New project for my new work

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Nav from './components/nav/Nav'
+import { Container } from './utils/Utils'
+
+function App() {
+
+  return (
+      <Nav/>    
+  )
+}
+
+export default App
